e7a04c416a5129cc476374c0220a3bb8a63b8c88
Cristian Lupascu <cristi.lupascu@gmail.com>
2013-03-17 23:28:03 +0200

    Solved composition scenario

---
+++ Composition/CS/Composition/Algorithm/Composition/HighPassSummingAggregator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm.Composition
+{
+	public class HighPassSummingAggregator : PointsAggregator
+	{
+		public HighPassSummingAggregator(IEnumerable<Measurement> measurements)
+			: base(measurements, new HighPassFilter(), new SummingStrategy())
+		{
+		}
+	}
+}
